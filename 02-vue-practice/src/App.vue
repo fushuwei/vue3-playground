9845298e371a91436add68a21a8e3fb3c7b5be38
fushuwei <331044761@qq.com>
2025-10-08 17:02:10 +0800

    增加路由管理

--- 02-vue-practice/src/App.vue
+++ 02-vue-practice/src/App.vue
@@ -3,12 +3,6 @@
 </script>
 
 <template>
-  <p><strong>当前路由:</strong> {{ $route.fullPath }}</p>
-
-  <h1>
-    <RouterLink to="/markdownEditor">Markdown Editor</RouterLink>
-  </h1>
-
   <main>
     <RouterView/>
   </main>
---
+++ 02-vue-practice/src/views/Layout.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <p><strong>当前路由:</strong> {{ $route.fullPath }}</p>
+
+  <h1>
+    <p>
+      <RouterLink to="/markdown-editor">Markdown Editor</RouterLink>
+    </p>
+  </h1>
+</template>
+
+<style scoped>
+
+</style>
